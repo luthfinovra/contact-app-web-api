318610fa0214bd1c1b01ed48793e9e299468145d
luthfinovra <lutvinovra@gmail.com>
2024-05-30 20:20:12 +0700

    fix jwt_secret

--- utils/middleware.js
+++ utils/middleware.js
@@ -12,7 +12,7 @@ const isLoggedIn = (req, res, next) => {
     console.log(token);
     const token = req.headers['authorization'].split(' ')[1];
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, 'your_jwt_secret' || process.env.JWT_SECRET);
         req.user = decoded;
         return next();
     } catch (error) {
